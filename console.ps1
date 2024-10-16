1ffb6aadd34634d3cf742e38717891f14f195957
Lars Loonen <lars.van.der.spek@vandoren.nl>
2024-10-16 12:13:48 +0200

    added fzf shortkey

--- console.ps1
+++ console.ps1
@@ -4,14 +4,13 @@ $check_installed = "choco.exe","git.exe","rg.exe","nvim.exe", "jq.exe", "zoxide.
 # carapace is installed with scoop
 
 foreach ($package in $check_installed) {
-    if (!(Get-Command -Name $package -ErrorAction SilentlyContinue)) {
-      Write-Host "$($package) not available"
+  if (!(Get-Command -Name $package -ErrorAction SilentlyContinue)) {
+    Write-Host "$($package) not available"
   }
 }
 
 # Aliases
 Set-Alias -Name vim -Value nvim
-Set-Alias -Name cd -Value z -Option AllScope
 Set-Alias -Name ex -Value explorer
 
 # Carapace options
@@ -25,5 +24,8 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Chord Ctrl+n -Function NextHistory 
 Set-PSReadLineKeyHandler -Chord Ctrl+p -Function PreviousHistory 
 
-# Init zoxide in PS
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' 
+
+# Init zoxide in PS, then set alias.
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+Set-Alias -Name cd -Value z -Option AllScope
